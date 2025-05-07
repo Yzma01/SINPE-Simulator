3c50d149ee28ee052b526f7d4090023348ea1216
Yzma01 <marchena.isma@gmail.com>
2025-05-06 21:55:47 -0600

    Revert "register client api create"
    This reverts commit 8fadcea7516ed8b5cb4104c409f04a351bacb6d6.

--- src/pages/server/controllers/client-controller.js
+++
@@ -1,9 +0,0 @@
-import {clientsRepo} from '@/pages/server/CRUD/clients-repo.js';
-
-export const getClientById=(req, res)=>{
-    clientsRepo._getClients(req,res);
-}
-
-export const addClient = (req, res)=>{
-    clientsRepo._addClient(req, res);
-}
